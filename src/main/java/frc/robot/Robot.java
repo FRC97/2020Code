48ca8734100741d1187ba9bbde5f3cf6e9e75c8f
dwssmn <58951982+dwssmn@users.noreply.github.com>
2020-02-07 16:24:39 -0500

    merging muhammed code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,7 +6,7 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot;
-//
+
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import com.revrobotics.CANSparkMax;
@@ -16,20 +16,9 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
-//import edu.wpi.first.wpilibj.Timer;
-//import edu.wpi.first.wpilibj.command.Scheduler;
-//import frc.robot.commands.ArcadeDrive;
-import frc.robot.subsystems.DriveTrain;
-//import frc.robot.subsystems.JoystickController;
-//import frc.robot.subsystems.PID_Drive;
-import frc.robot.subsystems.Shooter;
-/*import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import edu.wpi.first.wpilibj.SpeedControllerGroup;*/
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
@@ -38,27 +27,27 @@ import edu.wpi.first.wpilibj.SpeedControllerGroup;*/
  * project.
  */
 public class Robot extends TimedRobot {
-  public static DriveTrain driveTrain;
-  //public static OI m_oi;
-  public static Joystick m_stick;
-  public static Shooter shooter;
-  public RobotMap map;
-  //public ArcadeDrive m_ADrive;
-  //private final DifferentialDrive m_robotDrive = new DifferentialDrive(Left, Right);
-  //private final Timer m_timer = new Timer();
-  //public PID_Drive piddrive = new PID_Drive(1, 1, 1);
-  NetworkTable table;
-
   /**
    * This function is run when the robot is first started up and should be used
    * for any initialization code.
    */
-  private WPI_VictorSPX FR = new WPI_VictorSPX(RobotMap.FR);
-  private WPI_VictorSPX FL = new WPI_VictorSPX(RobotMap.FL);
-  private CANSparkMax testMotor = new CANSparkMax(10, MotorType.kBrushless);;
-  //private WPI_TalonSRX TestController = new WPI_TalonSRX(0);
-  private WPI_TalonSRX BR = new WPI_TalonSRX(RobotMap.BR);
-  private WPI_TalonSRX BL = new WPI_TalonSRX(RobotMap.BL);
+  private CANSparkMax FR = new CANSparkMax(RobotMap.FR, MotorType.kBrushless);
+  private CANSparkMax FL = new CANSparkMax(RobotMap.FL, MotorType.kBrushless);
+  private CANSparkMax BR = new CANSparkMax(RobotMap.BR, MotorType.kBrushless);
+  private CANSparkMax BL = new CANSparkMax(RobotMap.BL, MotorType.kBrushless);
+  
+  
+  // private WPI_TalonSRX TopShooterMotor = new WPI_TalonSRX(RobotMap.TS);
+  // private WPI_TalonSRX BottomShooterMotor = new WPI_TalonSRX(RobotMap.BS);
+
+  // private WPI_VictorSPX DeployClimbMotor = new WPI_VictorSPX(RobotMap.Dc);
+  // private WPI_VictorSPX RetractClimbMotor = new WPI_VictorSPX(RobotMap.Rc);
+
+  private WPI_VictorSPX GathererMotor = new WPI_VictorSPX(RobotMap.Gatherer);
+  private WPI_VictorSPX IndexerMotor = new WPI_VictorSPX(RobotMap.Indexer);
+  
+  //private [[TYPE_MOTORCONTROLLER]] TestController = new [[TYPE_MOTORCONTROLLER]](RobotMap.Test);
+
 
   private Joystick joy = new Joystick(0);
 
@@ -66,24 +55,25 @@ public class Robot extends TimedRobot {
   private SpeedControllerGroup left;
 
   private DifferentialDrive Drive;
+  
+  private boolean testMode = false;
 
   @Override
   public void robotInit() {
     // Inverted settings
-    FR.setInverted(false);
-    BR.setInverted(false);
-    FL.setInverted(true);
-    BL.setInverted(true);
+    // FR.setInverted(false);
+    // BR.setInverted(false);
+    // FL.setInverted(false);
+    // BL.setInverted(false);
 
     right = new SpeedControllerGroup(FR, BR);
-    left = new SpeedControllerGroup(BR, BL);
+    left = new SpeedControllerGroup(FL, BL);
 
     // Differential Driv Deadband percentage
     Drive = new DifferentialDrive(left, right);
     Drive.setDeadband(0.05);
 
-    // init encocder
-
+    // init encocder  
     /*
     RightMaster.setSensorPhase(true);
     RightMaster.setSensorPhase(false);
@@ -97,8 +87,15 @@ public class Robot extends TimedRobot {
   public void robotPeriodic() {
     SmartDashboard.putNumber("Joy Stick X", joy.getRawAxis(0));
     SmartDashboard.putNumber("Joy Stick Y", joy.getRawAxis(1));
-    SmartDashboard.putNumber("NEO Position", testMotor.getEncoder().getPosition());
-    SmartDashboard.putNumber("NEO Velocity", testMotor.getEncoder().getVelocity());
+    SmartDashboard.putNumber("Joy Stick Slider", joy.getRawAxis(3));
+    SmartDashboard.putNumber("Slider %-Value", sliderContrain());
+    SmartDashboard.putNumber("NEO BACK RIGHT", BR.get());
+    SmartDashboard.putNumber("NEO FRONT RIGHT", FR.get());
+    SmartDashboard.putNumber("NEO FRONT LEFT", FL.get());
+    SmartDashboard.putNumber("NEO BACK LEFT", BL.get());
+    SmartDashboard.putBoolean("TEST MOTOR TOGGLE", testMode);
+    //SmartDashboard.putNumber("NEO Position", testMotor.getEncoder().getPosition());
+    //SmartDashboard.putNumber("NEO Velocity", testMotor.getEncoder().getVelocity());
     //SmartDashboard.putNumber("Motor Controller", TestController.get());
   }
   
@@ -118,25 +115,51 @@ public class Robot extends TimedRobot {
   public void teleopPeriodic() {
     double speed = -contrain(joy.getRawAxis(1));
     double turn = contrain(joy.getRawAxis(0));
-    
-    Drive.arcadeDrive(speed, turn);
-    testMotor.set(speed);
-    //Scheduler.getInstance().run();
-    //piddrive.setSetpoint(getAngle(m_stick.getX(),m_stick.getY()));
-    //DriveTrain.m_drive.arcadeDrive(m_stick.getY()-0.1,constrain(getAngle(m_stick.getX(),m_stick.getY())));
-    //piddrive.execute(m_stick.getY()-0.1);
-    shooter.shoot(joy.getRawButtonPressed(0));
-    /*
-    if (m_stick.getX() < 0.1 && m_stick.getX() > -0.1) {
-      x_value = 0;
-    } else {
-      x_value = m_stick.getX();
+    Drive.arcadeDrive(speed*sliderContrain(), turn);
+
+    if (joy.getRawButton(2)){ 
+      GathererMotor.set(joy.getRawAxis(3));
     }
-    */
-    //m_ADrive.execute();
-    //m_robotDrive.arcadeDrive(m_stick.getY(), x_value);
-    //front_Left.set(ControlMode.PercentOutput, m_stick.getX());
-    //back_Left.set(ControlMode.PercentOutput, m_stick.getY()+0.1);
+    else{
+      GathererMotor.set(0);
+    }
+    if (joy.getRawButton(4)){ 
+      IndexerMotor.set(-.5);
+    }
+    else{
+      IndexerMotor.set(0);
+    }
+    
+    if (testMode){
+      if (joy.getRawButton(5)){
+        FR.set(0.5);
+        FL.set(0);
+        BR.set(0);
+        BL.set(0);
+      }
+      if (joy.getRawButton(6)){ 
+        FR.set(0);
+        FL.set(0.5);
+        BR.set(0);
+        BL.set(0);
+      }
+      if (joy.getRawButton(7)){ 
+        FR.set(0);
+        FL.set(0);
+        BR.set(0.5);
+        BL.set(0);
+      }
+      if (joy.getRawButton(8)){
+        FR.set(0);
+        FL.set(0);
+        BR.set(0);
+        BL.set(0.5);
+      }
+    }
+    if (joy.getRawButtonPressed(9)) {
+      testMode = !testMode;
+    }
+    //testMotor.set(speed);
   }
 
   @Override
@@ -156,5 +179,9 @@ public class Robot extends TimedRobot {
     else{
       return value;
     }
+  }
+  private double sliderContrain(){
+    double v = joy.getRawAxis(3) - 1; //-1 to 1 turns to -2 to 0
+    return Math.abs(v/2);
   }
 }
